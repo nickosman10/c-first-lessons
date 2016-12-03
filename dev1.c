dc7a52c7ae45946b07af4dec6ff1acf06d09a6e3
nickosman10 <nickosman10@gmail.com>
2016-12-03 22:12:43 +0200

    added original files

---
+++ dev1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, s, sum;
+
+    scanf("%d", &s);
+    printf("insert first number: ");
+
+    scanf("%d", &a);
+    printf("insert first number: ");
+    sum= a + s;
+
+    printf("The sum of %d and %d is %d \n", a ,s, sum);
+
+    return 0;
+}
+
+
+
+
---
+++ dev2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number, i;
+
+    printf("insert first number: ");
+    scanf("%d", &number);
+
+    for (i = 0; i < number; i ++)
+    {
+        printf("f***k");
+    }
+
+    return 0;
+}
+
+
+
+
